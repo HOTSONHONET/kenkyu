8a230ae25714140a843a5ecbdeb2ed9ad631023e
HOTSONHONET <hotsonhonet@gmail.com>
2025-09-11 19:30:26 +0530

    pushing working model for pdf downloader

--- child_paper_downloader.py
+++ child_paper_downloader.py
@@ -1,7 +1,13 @@
 import pymupdf
 from pprint import pprint
 import re
+import requests 
+import os
+from tqdm import tqdm
 
+
+class Config:
+    DOWNLOAD_PATH = "downloads"
 
 def main(filepath):
     doc = pymupdf.open(filepath)
@@ -9,7 +15,7 @@ def main(filepath):
     full_txt = ""
     for page in doc: full_txt += page.get_text()
 
-    refs_match = re.search(r'References|Bibliography)(.*)', full_txt, re.IGNORECASE | re.DOTALL)
+    refs_match = re.search(r'(References|Bibliography)(.*)', full_txt, re.IGNORECASE | re.DOTALL)
     refs_text = ""
     if refs_match: refs_text = refs_match.group(2).strip()
     
@@ -20,10 +26,45 @@ def main(filepath):
     
     references = [ref for ref in references if ref]
 
-    print("Extracted References: ")
+    refs = []
     for ref in references:
-        print(ref)
-        print("-"*100)
+        ref_list = ref.split("[")[1:]
+        for actual_ref in ref_list: refs.append("[" + actual_ref)
+        
+    # Collecting all the DOIs
+    dois = []
+    search_prefixes = ["arXiv:", "abs/"]
+    for ref in refs:
+        for search_prefix in search_prefixes:
+            if search_prefix in ref:
+                try:
+                    search_prefix_idx = ref.index(search_prefix)
+                    doi = ref[search_prefix_idx + len(search_prefix):].split(",")[0]
+                    title = ref[:100][4:] + ".pdf"
+                    dois.append({
+                        "name": title,
+                        "doi": doi
+                    })
+                except Exception as E:
+                    print(f"[ERROR] {E}")
+
+    pprint(dois)
+    print(f"Coverage: {len(dois)}/{len(refs)}")
+
+    # Creating a download directory
+    if not os.path.exists(Config.DOWNLOAD_PATH): os.mkdir(Config.DOWNLOAD_PATH)
+
+    url = "https://arxiv.org/pdf"
+    for ref_doi in tqdm(dois):
+        response = requests.get(f'{url}/{ref_doi["doi"]}')
+
+        if response.status_code == 200:
+            file_path = f"{Config.DOWNLOAD_PATH}/{ref_doi['name']}.pdf"
+            try:
+                with open(file_path, "wb") as file: file.write(response.content)
+            except Exception as E:
+                print(f"[ERROR] {E}")
+    
 
 
 
